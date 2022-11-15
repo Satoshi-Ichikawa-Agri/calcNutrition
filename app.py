b71c1c8c7df5e1bbc5be7231cabc4f26a751b4d9
Satoshi-Ichikawa-Agri <stayhungry.stayfoolish.satoshi@gmail.com>
2022-11-16 07:26:09 +0900

    modify and refactoring app.py

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ pyocrを使用して、食品ラベルのkcal部分を読み取る
 保存先からその値を取得
 「◯/◯/◯(実行した日の年月日)の摂取カロリーは◯◯kcalです。」と表示
 """
-import sys
-import csv
 import datetime
-import os
+import sys
 
 from PIL import Image
 import pyocr
@@ -16,7 +14,8 @@ import pyocr.builders
 
 # オブジェクトの生成
 tools = pyocr.get_available_tools()
-# 読み込んだ画像に文字列が無い場合、エラーを返す
+
+# get_available_toolsに何も存在しない場合、エラーを返す
 if len(tools) == 0:
     print("No OCR tool found")
     sys.exit(1)
@@ -24,6 +23,11 @@ if len(tools) == 0:
 # tesseractを格納
 tool = tools[0]
 
+# pyocr.buildersを生成する
+builder = pyocr.builders.DigitBuilder(tesseract_layout=6)
+
+# 本日日付を取得
+today = str(datetime.date.today())
 
 # 使用する画像のpathをlistに格納する
 data = [
@@ -33,51 +37,57 @@ data = [
     "images/04.png",
     "images/05.png",
 ]
-# data = os.listdir("images")
-# print(data)
-# print(type(data))
 
 
-# 本日日付を取得
-today = datetime.date.today()
-today = str(today)
-
-# 03.pngが読み込めないので、リサイズして挑戦(拡大)
-img03 = Image.open(data[2])
-resize_img03 = img03.resize((500, 400))
-resize_img03.save("images/03_new.png")
-data[2] = "images/03_new.png"
-
-# 合計変数
-sum = 0
-
-# 画像の解析結果を格納
-for text in data:
-    if text == "images/03_new.png":
-        continue
-    # print(text)
-    # print(type(text))
-    img = Image.open(text)
-    # 分析
-    num = tool.image_to_string(
-        img,
-        lang="jpn",
-        builder=pyocr.builders.DigitBuilder(),
-    )
-
-    num = int(num)
-    print(num)
-    print(type(num))
-    sum += num
-print(sum)
+def reading_calculation(data, lang, builder):
+    """画像の解析結果を格納
+    data: 読み取りたいimageデータのpathのList
+    lang: 読み取る言語 jpn/eng
+    builder: builderオブジェクト Digit/Textなどある
+    return: 合計
+    """
+    # 合計変数
+    sum = 0
+    # 画像パスの分だけ読み取りを行い、読み取り結果をnumに格納し、sumで加算していく
+    for read_data in data:
+        # 画像読込
+        img = Image.open(read_data)
+        # 分析
+        num = int(
+            tool.image_to_string(
+                img,
+                lang=lang,
+                builder=builder,
+            )
+        )
+        # 読み取り結果を加算
+        sum += num
+    return str(sum)
 
 
-# Textへの書込み
-with open("text.txt", mode="w") as f:
-    f.writelines(str(sum))
+def text_write(content):
+    """Textへ書込み
+    受け取った値をテキストに書き込む
+    """
+    with open("text.txt", mode="w") as f:
+        f.writelines(content)
 
 
-# Textを読込、結果を出力
-with open("text.txt", mode="r") as f:
-    result = f.read()
-    print(today + "の摂取カロリーは" + result + "kcalです。")
+def text_read():
+    """Textを読込、結果を出力
+    受け取ったテキストを読み込むに書き込む
+    """
+    with open("text.txt", mode="r") as f:
+        reading_result = f.read()
+        print(today + "の摂取カロリーは" + reading_result + "kcalです。")
+
+
+def main():
+    result = reading_calculation(data, "jpn", builder)
+    return result
+
+
+if __name__ == "__main__":
+    result = main()
+    text_write(result)
+    text_read()
